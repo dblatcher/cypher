8c7e8f5f23774af7b2a40e400e207d10d18259b8
dblatcher <dv.bltchr@gmail.com>
2021-12-03 08:53:49 +0000

    add jest test

---
+++ tests/cypher.test.js
@@ -0,0 +1,21 @@
+const  Cypher =  require ("../src/cypher");
+
+cypher = new Cypher({
+    "T": "+",
+    "s": "^",
+},{
+    upperCase: true
+});
+
+const message = "This is the message.";
+
+test ('replaces letter T with a plus',()=>{
+    const coded = cypher.encode("Tt");
+    expect(coded).toBe("++");
+});
+
+test ('can decode back to uppercase', ()=>{
+    const coded = cypher.encode(message);
+    const decode = cypher.decode(coded)
+    expect(decode).toBe(message.toUpperCase());
+});
