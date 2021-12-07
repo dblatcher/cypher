df3383b3f1c06c026627c85abd4adbc9789031fa
dblatcher <dv.bltchr@gmail.com>
2021-12-07 16:17:02 +0000

    more descriptive tests

--- tests/cypher.test.js
+++ tests/cypher.test.js
@@ -1,42 +1,42 @@
 import Cypher from "../src/js/cypher"
 
-const cypher = new Cypher({
-    "T": "+",
-    "s": "^",
-},{
-    upperCase: true
-});
-
 const message = "This is the message.";
 
-test ('replaces letter T with a plus',()=>{
-    const coded = cypher.encode("Tt");
-    expect(coded).toBe("++");
+test('It can replace upper and lower case letters.', () => {
+    const testPartialCypher = new Cypher({
+        "T": "Z",
+        "s": "X",
+    }, {
+        upperCase: true
+    });
+    const coded = testPartialCypher.encode("Tt");
+    expect(coded).toBe("ZZ");
 });
 
-test ('can decode back to uppercase', ()=>{
+test('can decode back to uppercase', () => {
+    const cypher = new Cypher(Cypher.makeRandomAlphaKeyMap());
     const coded = cypher.encode(message);
     const decode = cypher.decode(coded)
     expect(decode).toBe(message.toUpperCase());
 });
 
-test ('can produce an empty alphabet key', ()=> {
+test('can produce an empty alphabet key', () => {
     const keyMap = Cypher.makeEmptyAlphaKeyMap()
-    expect (Object.keys(keyMap)).toEqual(expect.arrayContaining(Cypher.alphabet))
+    expect(Object.keys(keyMap)).toEqual(expect.arrayContaining(Cypher.alphabet))
 })
 
-test ('can produce an random alphabet key', ()=> {
+test('can produce an random alphabet key', () => {
     const keyMap = Cypher.makeRandomAlphaKeyMap()
 
     const keys = Object.keys(keyMap);
-    const values =keys.map(key=>keyMap[key]);
+    const values = keys.map(key => keyMap[key]);
 
-    expect (keys.length).toEqual(Cypher.alphabet.length)
-    expect (keys).toEqual(expect.arrayContaining(Cypher.alphabet))
-    expect (values).toEqual(expect.arrayContaining(Cypher.alphabet))
+    expect(keys.length).toEqual(Cypher.alphabet.length)
+    expect(keys).toEqual(expect.arrayContaining(Cypher.alphabet))
+    expect(values).toEqual(expect.arrayContaining(Cypher.alphabet))
 })
 
-test ('a random cypher can encode and decode back to the same message', ()=>{
+test('a random cypher can encode a message with punctuation and decode back to uppercase version of the message.', () => {
 
     const cypher = new Cypher(Cypher.makeRandomAlphaKeyMap());
     const message = "This is our test message. It has punctuation!";
