025b53ded16620f2b79a874c1ba05e00d10e068f
dblatcher <dv.bltchr@gmail.com>
2021-12-09 14:03:48 +0000

    add stylesheets for game areas

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,9 @@
 import Cypher from "./cypher"
 import Puzzle from "./Puzzle";
+import "../scss/main.scss"
 
-
-let message = "This is my puzzle!";
-// let message = "Now that the table has been created and the template defined, we use JavaScript to insert rows into the table, with each row being constructed using the template as its basis.";
+// let message = "This is my puzzle!";
+let message = "Now that the table has been created and the template defined, we use JavaScript to insert rows into the table, with each row being constructed using the template as its basis.";
 // let message = "the."
 
 const puzzle = new Puzzle({
